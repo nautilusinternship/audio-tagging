8f3c7d2fb2f8598dd81367721d1f064e5f8ed8c7
carinalewandowski <56599238+carinalewandowski@users.noreply.github.com>
2020-07-14 20:46:29 -0400

    Add files via upload
    barebones code for app layer file, obvi doesn't do anything yet b/c the html files & db module don't exist yet

---
+++ app.py
@@ -0,0 +1,29 @@
+# --------------------------------------------------------
+# app.py: app layer program for Audio Tagging application
+# --------------------------------------------------------
+
+from flask import Flask, request, redirect, make_response, render_template
+app = Flask(__name__)
+
+# index/home view
+@app.route('/')
+def index():
+    # embed audio clip to be played
+    # display input fields
+    # redirect to confirmation view
+    html = render_template('index.html')
+    response = make_response(html)
+    return response
+
+# confirmation view
+@app.route('/success')
+def success():
+    # parse user inputs as list
+    descriptors = request.form.getlist('input')
+    # send descriptors to db layer file to be manipulated/to update db
+
+    # display confirmation message to verify success of user inputs being processed
+    # view will maybe have a button to return home to repeat process on another audio file?
+    html = render_template('success.html')
+    response = make_response(html)
+    return response
