388c2d3ca590c642a308dfc544e0aaacb231e7d8
Anabelle <bellech88@gmail.com>
2020-07-15 15:09:48 -0700

    added basic form of 3 words and results page

--- app.py
+++ app.py
@@ -15,18 +15,27 @@ def index():
     response = make_response(html)
     return response
 
+# https://www.tutorialspoint.com/flask/flask_sending_form_data_to_template.htm
+@app.route('/results',methods = ['POST', 'GET'])
+def results():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("results.html",result = result)
+
+
 # confirmation view
 @app.route('/success')
 def success():
     # parse user inputs as list
-    descriptors = request.form.getlist('input')
+    # descriptors = request.form.getlist('input')
     # send descriptors to db layer file to be manipulated/to update db
 
     # display confirmation message to verify success of user inputs being processed
     # view will maybe have a button to return home to repeat process on another audio file?
-    html = render_template('success.html')
-    response = make_response(html)
-    return response
+    # html = render_template('success.html')
+    # response = make_response(html)
+    # return response
+    return 'success'
 
 if __name__ == "__main__":
     app.run()
